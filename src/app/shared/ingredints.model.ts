879e3f1dfc3e25304656dd07324c75ef80e19a89
barakat <barakat.8163@gmail.com>
2018-09-05 01:27:15 +0300

    adding shopping list edit

---
+++ src/app/shared/ingredints.model.ts
@@ -0,0 +1,5 @@
+export class Ingredint {
+;
+ 
+ constructor(      public name : string ,public amount : number  ){}
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredint} from '../shared/ingredints.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredints : Ingredint[] =[
+  new Ingredint('Apple',5),
+  new Ingredint('Tomatoes',10),
+];
   constructor() { }
 
   ngOnInit() {
